92448d4c56c146a3db78d6de895525357d109925
Shekharpawar1 <86783159+Shekharpawar1@users.noreply.github.com>
2022-05-19 13:30:35 +0530

    used jwt for token auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,13 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken");
 //validator
 const {
   body,
   validationResult
 } = require('express-validator');
-
+const JWT_SECRET="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elementum quis leo vel rhoncus. Praesent felis mi, accumsan venenatis scelerisque sit amet, sodales in erat. In dapibus vel velit sodales commodo. Maecenas mollis enim non elit vulputate, eget maximus turpis tempus. Pellentesque justo ex, facilisis id purus eu, dictum vestibulum mi. Suspendisse neque ante, dictum eu lacus at, hendrerit dapibus arcu. Mauris aliquam mauris at lacus convallis tristique. Donec in ex sapien. Sed luctus dignissim efficitur. Nunc rutrum sapien vitae scelerisque mollis. Etiam pellentesque nec nulla ac faucibus. Nulla a nisl tellus. Aliquam sed nisl non dui consectetur sagittis. Proin lectus neque, vestibulum eu aliquet sed, viverra at nulla. Nam facilisis, tellus sed dignissim tincidunt, neque lacus lobortis felis, elementum scelerisque quam sem at augue. Sed sed eros felis. "
 router.post("/createUser", [body("name", "Min length of Name should be 3").isLength({
   min: 3
 }), body('email', "Email is not valid ").isEmail(), body("password", "password length should be 6 or more characters").isLength({
@@ -32,7 +33,10 @@ router.post("/createUser", [body("name", "Min length of Name should be 3").isLen
         errors: "Email is already registered"
       })
     }
+    //hashing password
+    //this would create a salt which would be stored in the db
     const salt=await bcrypt.genSalt(10),
+    //with the help of the salt the below line would hash the password
           hashedPassword= await bcrypt.hash(req.body.password,salt);
     user =await User.create({
 
@@ -43,8 +47,10 @@ router.post("/createUser", [body("name", "Min length of Name should be 3").isLen
     // .then(user => res.json(user)).catch((err) => res.json({
     //   error: "please enter a new email" }
     // ));
-    res.send(user)
-  } catch(err){
+    const data={user:{id:user.id}};
+    let  authtoken = jwt.sign(data, JWT_SECRET);
+    res.send(authtoken)
+  }catch(err){
     console.log(err);
     return res.status(500).json(" Internal Server error")
   }
